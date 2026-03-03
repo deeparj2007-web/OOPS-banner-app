63b864521ca4028acb03e4457dd901c3047c643e
deeparj2007-web <deepa.rj.2007@gmail.com>
2026-03-03 22:30:03 +0530

    added UC files

---
+++ OOPSBannerUC1.java
@@ -0,0 +1,20 @@
+/**
+ * OOPSBannerApp
+ * 
+ * UC1: Print OOPS to Console
+ * 
+ * This program prints the literal text "OOPS" to the console.
+ * 
+ * @author YourName
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * Main method - Entry point of the Java application
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
